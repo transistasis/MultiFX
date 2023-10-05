33ddfe2f646508a7743ddc806031e3c472a013b1
transistasis <46616290+transistasis@users.noreply.github.com>
2023-10-04 22:20:40 -0400

    Added the JUCE DSP module and defined effects to be used

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -56,7 +56,21 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    //==============================================================================
+    enum class DSP_Option {
+        Phase,
+        Chorus,
+        OverDrive,
+        LadderFilter,
+        END_OF_LIST
+    };
+
 private:
     //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MultiFXAudioProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MultiFXAudioProcessor)
+
+    //==============================================================================
+    juce::dsp::Phaser<float> phaser;
+    juce::dsp::Chorus<float> chorus;
+    juce::dsp::LadderFilter<float> overdrive, ladderFilter;
 };
